3856e0f4a7eb3711ab20c9d3a5c426a798d37ba7
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-03-15 10:13:31 +0300

    Kullanıcı VC
    Kullanıcı arayüzü tasarımı yapıldı

--- Codes/SwiftMachineLearningImageMatch/SwiftMachineLearningImageMatch/ViewController.swift
+++ Codes/SwiftMachineLearningImageMatch/SwiftMachineLearningImageMatch/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageDescLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate=self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing=true
+        present(picker,animated: true,completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image=info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+       
+    }
+    
 
+    
+ 
+    
 }
 
