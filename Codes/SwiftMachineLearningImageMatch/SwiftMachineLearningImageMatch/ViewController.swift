b88cb76e267cd710fdbc4d844bc61c6793febd63
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-03-15 10:05:43 +0300

    Proje oluşturuldu
    Proje dosyaları oluşturuldu

---
+++ Codes/SwiftMachineLearningImageMatch/SwiftMachineLearningImageMatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMachineLearningImageMatch
+//
+//  Created by Burak Emre gündeş on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
